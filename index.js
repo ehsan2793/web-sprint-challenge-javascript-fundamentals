281e14159e120999d093a848bce9e6e7c45a5db4
ehsan2793 <ehsan2793@gmail.com>
2021-07-23 00:48:38 -0700

    making sure push is succesful, 1st and 2nd task of index.js are completed and are passing, i will continute the work in the morning

--- index.js
+++ index.js
@@ -17,8 +17,8 @@ myFunction();
 
 //🚀🚀🚀 ⬇️ 📝 Explanation ⬇️ 📝 🚀🚀🚀: 
 
-
-
+// when we define one function with in the an another function the inner function has access to outer function as well as global scope variables and this is called lexical scope function
+// we see that internal variable is defined in the outer function myFunction and nested function that is inside has access to outer function variables.
 
 
 /* 🚀🚀🚀 Task 2: Counter 🚀🚀🚀 */
@@ -28,11 +28,14 @@ myFunction();
     
 For example, `summation(4)` should return 10 because 1+2+3+4 is 10. Note, you may use a for loop for this function if you wish */
 
-function summation(/*Your Code Here*/) {
-  /*Your Code Here*/
-
+function summation(number) {
+  let counter = 0;
+  for (let i = 1; i <= number; i++) {
+    counter += i
   }
- 
+return counter;
+  }
+ console.log(summation(7))
 
 // 🦁🦁🦁 Topic 2: ADVANCED Array Methods 🦁🦁🦁
 // Given this zoo data from around the United States, follow the instructions below. Use the specific array methods in the requests below to solve the problems.
